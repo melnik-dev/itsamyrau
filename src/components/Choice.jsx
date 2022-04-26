2ff63b072ef3ea3a03e8a3550ed40a55d5df71cb
Aleksandr <for.melnik.dev@gmail.com>
2022-04-26 20:06:25 +0300

    написал трикомпаненты

---
+++ src/components/Choice.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+const Choice = () => {
+  return (
+    <section classNameName="section__choice">
+      <div className="choice__content">
+        <div className="choice__header">
+          <div className="choice__wrapp-title">
+            <h1 className="choice__title">Chats</h1>
+            <p className="choice__subtitle">Recent Chats</p>
+          </div>
+          <button className="choice__btn btn">
+            <i className="icons icon-plus"></i>Create new Chat
+          </button>
+        </div>
+
+        <div className="choice__search">
+          <i className="icons icon-search"></i>
+          <input className="choice__input-search" type="search" />
+          <button className="choice__btn-search">Search</button>
+        </div>
+        <div className="choice__chat-wrapper">
+          <div className="choice__chat">
+            <div className="choice-chat__head">
+              <div className="choice-chat__avatar">
+                <picture>
+                  <source srcset="img/hermione.webp" type="image/webp" />
+                  <img src="img/hermione.jpg" alt="hermione" className="choice-chat__avatar-img" />
+                </picture>
+              </div>
+
+              <div className="choice-chat__title-wrapp">
+                <div className="choice-chat__title">
+                  <h4 className="choice-chat__name">Hermione Granger</h4>
+                  <span className="choice-chat__stat">writes...</span>
+                </div>
+                <span className="choice-chat__time">1 minute ago</span>
+              </div>
+            </div>
+            <div className="choice-chat__content">
+              <span className="choice-chat__content-text">Hello how are you</span>
+              <span className="choice-chat__content-msg">2</span>
+            </div>
+          </div>
+
+          <div className="choice__chat choice__chat-active">
+            <div className="choice-chat__head">
+              <div className="choice-chat__avatar">
+                <picture>
+                  <source srcset="img/ronwezlle.webp" type="image/webp" />
+                  <img src="img/ronwezlle.jpg" alt="Ron" className="choice-chat__avatar-img" />
+                </picture>
+              </div>
+
+              <div className="choice-chat__title-wrapp">
+                <div className="choice-chat__title">
+                  <h4 className="choice-chat__name">Ron Weasley</h4>
+                  <span className="choice-chat__stat">last online 5 hours ago</span>
+                </div>
+                <span className="choice-chat__time">3 days ago</span>
+              </div>
+            </div>
+            <div className="choice-chat__content">
+              <span className="choice-chat__content-text">Hello! I tweaked everything you asked. I am sending the finished file.</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Choice;
---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header classNameName="section__header">
+      <div classNameName="header__avatar avatar">
+        <picture>
+          <source srcset="img/garri_potter.webp" type="image/webp" />
+          <img classNameName="avatar__img" src="img/garri_potter.jpg" alt="garri potter" />
+        </picture>
+        <span classNameName="avatar__name">Garri Potter</span>
+      </div>
+
+      <nav classNameName="header__menu menu__body">
+        <ul classNameName="menu__list">
+          <li classNameName="menu__item menu__item--active">
+            <a classNameName="menu__link" href="#">
+              <i classNameName="icons icon-houm"></i> Home
+            </a>
+          </li>
+          <li classNameName="menu__item">
+            <a classNameName="menu__link" href="#">
+              <i classNameName="icons icon-chats"></i>Chat
+            </a>
+          </li>
+          <li classNameName="menu__item">
+            <a classNameName="menu__link" href="#">
+              <i classNameName="icons icon-chanels"></i>Channels
+            </a>
+          </li>
+          <li classNameName="menu__item">
+            <a classNameName="menu__link" href="#">
+              <i classNameName="icons icon-contacts"></i>Contact
+            </a>
+          </li>
+          <li classNameName="menu__item">
+            <a classNameName="menu__link" href="#">
+              <i classNameName="icons icon-setting"></i>Settings
+            </a>
+          </li>
+
+          <li classNameName="menu__item">
+            <a classNameName="menu__link" href="#">
+              {" "}
+              <i classNameName="icons icon-logout"></i>Log out
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
